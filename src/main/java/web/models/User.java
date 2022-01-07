de419e42b9b6923b7a0fae7c4abb67a591456ca4
Kristina <kitty_kris@bk.ru>
2022-01-07 09:10:55 +0300

    Set initialize when add role ar user

--- src/main/java/web/models/User.java
+++ src/main/java/web/models/User.java
@@ -43,7 +43,7 @@ public class User implements UserDetails {
     @JoinTable(name = "users_roles"
             , joinColumns = @JoinColumn(name = "user_id")
             , inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<Role> roles = new HashSet<>();
+    private Set<Role> roles;
 
     public User() {
     }
