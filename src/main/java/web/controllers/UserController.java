74b89f2c133158d575d8c9aec0a1741aaf786056
Kristina <kitty_kris@bk.ru>
2022-01-09 02:47:02 +0300

    left only user controller of user's representation

--- src/main/java/web/controllers/UserController.java
+++ src/main/java/web/controllers/UserController.java
@@ -4,27 +4,19 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import web.models.User;
 
 import java.security.Principal;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 
-	@GetMapping("/user")
+	@GetMapping
 	public String userDetails(Model model, Principal principal) {
 		User loginedUser = (User) ((Authentication) principal).getPrincipal();
 		model.addAttribute("userDetails", loginedUser);
 		return "userDetails";
-	}
-
-    @GetMapping("/login")
-    public String loginPage() {
-        return "login";
-    }
-
-	@GetMapping("/")
-	public String home() {
-		return "redirect:/login";
 	}
 }
