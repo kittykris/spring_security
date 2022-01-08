6593b61b16408928ff7d8e5589384bf49e6b0e1d
Kristina <kitty_kris@bk.ru>
2022-01-09 02:43:14 +0300

    add representation of user in controller

--- src/main/java/web/controllers/UserController.java
+++ src/main/java/web/controllers/UserController.java
@@ -1,19 +1,21 @@
 package web.controllers;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import web.models.User;
+
+import java.security.Principal;
 
 @Controller
 public class UserController {
 
 	@GetMapping("/user")
-	public String userDetails(@ModelAttribute("user") User user, Model model) {
-		model.addAttribute("userDetails", user);
-		model.addAttribute("userDetailsRoles", user.getRoles());
-		return "user";
+	public String userDetails(Model model, Principal principal) {
+		User loginedUser = (User) ((Authentication) principal).getPrincipal();
+		model.addAttribute("userDetails", loginedUser);
+		return "userDetails";
 	}
 
     @GetMapping("/login")
