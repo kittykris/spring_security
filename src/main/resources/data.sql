57532bcc17d78c0e5390cfb50563d4a62f6506ae
Kristina <kitty_kris@bk.ru>
2022-01-09 07:03:34 +0300

    fill users.html

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,8 @@
 insert into roles values (1, 'ROLE_USER'), (2, 'ROLE_ADMIN');
 insert into users value (1, 25, 'user', 'user', 'user', 'user', 'user');
+#password "user"
 insert into users value (2, 30, 'admin', 'admin', 'admin', 'admin', 'admin');
-insert into users_roles values (1, 1), (2, 2);
+#password "admin"
+insert into users value (3, 30, 'крис', 'крис', 'крис', 'user1', 'user1');
+#password "user1"
+insert into users_roles values (1, 1), (2, 2), (3, 1), (3, 2);
