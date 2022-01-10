310ea901d9382992f86af03edc2bd966b2b6cada
Kristina <kitty_kris@bk.ru>
2022-01-10 22:03:07 +0300

    delete not used imports

--- src/main/java/web/controllers/UserController.java
+++ src/main/java/web/controllers/UserController.java
@@ -4,7 +4,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import web.models.User;
 
 import java.security.Principal;
