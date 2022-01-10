45196ace0ab159dd20a9f613e31e10df8d083706
Kristina <kitty_kris@bk.ru>
2022-01-10 21:01:09 +0300

    add findByRoleName method and methods for adding and deleting Role to table

--- src/main/java/web/dao/RoleDao.java
+++ src/main/java/web/dao/RoleDao.java
@@ -7,4 +7,10 @@ import java.util.Set;
 public interface RoleDao {
 
     Set<Role> allRoles();
+
+    Role findRoleByName(String roleName);
+
+    void addRole(Role role);
+
+    void deleteRole(Role role);
 }
--- src/main/java/web/dao/RoleDaoImp.java
+++ src/main/java/web/dao/RoleDaoImp.java
@@ -2,11 +2,13 @@ package web.dao;
 
 import org.springframework.stereotype.Repository;
 import web.models.Role;
+import web.models.User;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Repository
@@ -21,5 +23,25 @@ public class RoleDaoImp implements RoleDao {
         return new HashSet<>(entityManager
                 .createQuery("SELECT role from Role role", Role.class)
                 .getResultList());
+    }
+
+    @Override
+    public Role findRoleByName(String roleName) {
+        return entityManager.createQuery("select r from Role r where r.name =: roleName", Role.class)
+                .setParameter("roleName", roleName).getSingleResult();
+    }
+
+    @Override
+    public void addRole(Role role) {
+        if (!entityManager.contains(role)) {
+            entityManager.persist(role);
+        }
+    }
+
+    @Override
+    public void deleteRole(Role role) {
+        if (entityManager.contains(role)) {
+            entityManager.remove(role);
+        }
     }
 }
