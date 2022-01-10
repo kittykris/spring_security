4b1c226e0dff4233b10c2c6958b1ac70abe79c1d
Kristina <kitty_kris@bk.ru>
2022-01-10 06:19:26 +0300

    stay only redirect on login page

--- src/main/java/web/controllers/MainController.java
+++ src/main/java/web/controllers/MainController.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping("/login")
-    public String loginPage() {
-        return "login";
-    }
-
     @GetMapping("/")
     public String home() {
         return "redirect:/login";
