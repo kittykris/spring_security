e91daa1c07a2fed93941134216939f0429700814
Kristina <kitty_kris@bk.ru>
2022-01-10 22:04:33 +0300

    add presentation of error 403

--- src/main/java/web/controllers/MainController.java
+++ src/main/java/web/controllers/MainController.java
@@ -1,7 +1,10 @@
 package web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class MainController {
@@ -9,5 +12,18 @@ public class MainController {
     @GetMapping("/")
     public String home() {
         return "redirect:/login";
+    }
+
+
+    @GetMapping("/403")
+    public String getAccessDenied(Model model, Principal principal) {
+        if (principal != null) {
+            model.addAttribute("msg", "Hi " + principal.getName()
+                    + ", sorry, but you do not have permission to access this page!");
+        } else {
+            model.addAttribute("msg",
+                    "Sorry, but You do not have permission to access this page!");
+        }
+        return "/403";
     }
 }
