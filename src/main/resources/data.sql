b86479a53fb559a90bcd9ef8521e1d36f34c7723
Kristina <kitty_kris@bk.ru>
2022-01-10 06:25:02 +0300

    add user1

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,6 +3,6 @@ insert into users value (1, 25, 'user', 'user', 'user', 'user', 'user');
 #password "user"
 insert into users value (2, 30, 'admin', 'admin', 'admin', 'admin', 'admin');
 #password "admin"
-insert into users value (3, 30, 'крис', 'крис', 'крис', 'user1', 'user1');
+insert into users value (3, 30, 'user1', 'user1', 'user1', 'user1', 'user1');
 #password "user1"
 insert into users_roles values (1, 1), (2, 2), (3, 1), (3, 2), (2, 1);
