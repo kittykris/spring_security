68cc6dbd5bc549863260010fedea745b2cb34f72
Kristina <kitty_kris@bk.ru>
2022-01-10 07:19:19 +0300

    fill controller

--- src/main/java/web/controllers/AdminController.java
+++ src/main/java/web/controllers/AdminController.java
@@ -1,16 +1,12 @@
 package web.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import web.models.User;
 import web.service.RoleService;
 import web.service.UserService;
-
-import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/admin")
@@ -18,9 +14,6 @@ public class AdminController {
 
     private UserService service;
     private RoleService roleService;
-
-    @Autowired
-    private PasswordEncoder bCryptEncoder;
 
     @Autowired
     public AdminController(UserService service, RoleService roleService) {
@@ -36,42 +29,34 @@ public class AdminController {
 
     @GetMapping("/new_add")
     public String viewNewUser(Model model) {
-        model.addAttribute("newUser", new User());
         model.addAttribute("allRoles", roleService.allRoles());
+        model.addAttribute("newUser", new User());
         return "addUser";
     }
 
     @PostMapping("/save")
-    public String addNewUser(@Valid @ModelAttribute User user, BindingResult result) {
-        if (result.hasErrors()) {
-            System.out.println("error");
-            user.setPassword(bCryptEncoder.encode(user.getPassword()));
-            return "addUser";
-        }
+    public String addNewUser(@ModelAttribute User user) {
         service.addUser(user);
-        return "redirect:/users";
+        return "redirect:/admin/users";
     }
 
     @GetMapping("/{id}/edit")
     public String editUser(@PathVariable("id") long id, Model model) {
+        model.addAttribute("allRoles",roleService.allRoles() );
         model.addAttribute("updateUser", service.getUserById(id));
         return "updateUser";
     }
 
     @PatchMapping("/{id}")
-    public String updateUser(@PathVariable("id") long id, @Valid @ModelAttribute User user, BindingResult result) {
-        if (result.hasErrors()) {
-            System.out.println("error");
-            return "updateUser";
-        }
+    public String updateUser(@PathVariable("id") long id, @ModelAttribute User user) {
         service.updateUser(id, user);
-        return "redirect:/users";
+        return "redirect:/admin/users";
     }
 
     @DeleteMapping("/{id}")
     public String deleteUser(@PathVariable long id) {
         service.deleteUser(id);
-        return "redirect:/users";
+        return "redirect:/admin/users";
     }
 
 }
