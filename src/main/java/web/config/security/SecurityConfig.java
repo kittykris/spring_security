ee6f1a89dd739e16ae8dce795dc1c58b869fdd64
Kristina <kitty_kris@bk.ru>
2022-01-11 00:40:50 +0300

    add bcrypt password encoder

--- src/main/java/web/config/security/SecurityConfig.java
+++ src/main/java/web/config/security/SecurityConfig.java
@@ -9,7 +9,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import web.config.security.handlers.CustomAccessDeniedHandler;
 import web.config.security.handlers.LoginSuccessHandler;
 
@@ -47,15 +48,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .successHandler(loginSuccessHandler);
     }
-        //todo bcrypt encoder
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder(12);
-//    }
 
     @Bean
-    public static NoOpPasswordEncoder passwordEncoder() {
-        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
     }
 
     @Bean
