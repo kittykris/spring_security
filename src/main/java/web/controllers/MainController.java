8208e4cfa251dda7e5d5bde28ab7a25db1cd7606
Kristina <kitty_kris@bk.ru>
2022-01-13 01:43:13 +0300

    delete not used method

--- src/main/java/web/controllers/MainController.java
+++ src/main/java/web/controllers/MainController.java
@@ -2,12 +2,9 @@ package web.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import web.service.RoleService;
 import web.service.UserService;
-
-import java.security.Principal;
 
 @Controller
 public class MainController {
@@ -26,18 +23,5 @@ public class MainController {
     @GetMapping("/")
     public String home() {
         return "redirect:/login";
-    }
-
-
-    @GetMapping("/403")
-    public String getAccessDenied(Model model, Principal principal) {
-        if (principal != null) {
-            model.addAttribute("msg", "Hi " + principal.getName()
-                    + "! Sorry, but you do not have permission to access this page!");
-        } else {
-            model.addAttribute("msg",
-                    "Sorry, but You do not have permission to access this page!");
-        }
-        return "/403";
     }
 }
