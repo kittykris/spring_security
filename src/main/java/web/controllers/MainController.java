4d8879fb063438c86b2778555def6d59f376bddf
Kristina <kitty_kris@bk.ru>
2022-01-12 23:22:37 +0300

    add initializing users and roles in constructor

--- src/main/java/web/controllers/MainController.java
+++ src/main/java/web/controllers/MainController.java
@@ -1,13 +1,27 @@
 package web.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import web.service.RoleService;
+import web.service.UserService;
 
 import java.security.Principal;
 
 @Controller
 public class MainController {
+
+    private final UserService userService;
+    private final RoleService roleService;
+
+    @Autowired
+    public MainController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+        this.roleService.addDefaultRoles();
+        this.userService.addDefaultUsers();
+    }
 
     @GetMapping("/")
     public String home() {
